68d2b43e877964cab51c537fbd623526a11b820d
Jan Skiba <jan.skiba@dkfz-heidelberg.de>
2025-11-13 21:46:53 +0000

    Without solution

--- src/bb-beam/beam.py
+++ src/bb-beam/beam.py
@@ -1,8 +1,7 @@
-from typing import Any, AsyncGenerator, List, Literal, Optional
+from typing import Any, List, Literal, Optional
 from uuid import UUID, uuid4
 
 import httpx
-import httpx_sse
 from pydantic import BaseModel, ConfigDict, Field
 
 
@@ -46,38 +45,13 @@ class BeamClient:
         self.client = httpx.AsyncClient(headers={"Authorization": f"ApiKey {app_id} {beam_apikey}"})
 
     async def post_beam_task(self, task: BeamTask):
-        json = task.model_dump_json(by_alias=True)
-        response = await self.client.post(f"{self.base_url}/v1/tasks", content=json)
-        response.raise_for_status()
+        pass
 
     async def get_beam_tasks(self) -> list[BeamTask]:
-        response = await self.client.get(f"{self.base_url}/v1/tasks", params={
-            "filter": "todo",
-            "wait_time": "1s"
-        })
-        response.raise_for_status()
-        tasks_data = response.json()
-        beam_tasks = [BeamTask.model_validate(task_data) for task_data in tasks_data]
-        return beam_tasks
+        return []
 
     async def answer_task(self, task: BeamTask, body: str, workstatus: BeamWorkStatus = "succeeded", metadata: Optional[Any] = None):
-        result = BeamResult(
-            task=task.id,
-            from_=self.app_id,
-            to=[task.from_],
-            body=body,
-            status=workstatus,
-            metadata=metadata
-        )
-        response = await self.client.put(f"{self.base_url}/v1/tasks/{task.id}/results/{self.app_id}", content=result.model_dump_json(by_alias=True))
-        response.raise_for_status()
+        pass
 
     async def get_task_results(self, task_id: UUID) -> list[BeamResult]:
-        response = await self.client.get(f"{self.base_url}/v1/tasks/{task_id}/results", params={
-            "wait_time": "1s"
-        })
-        if response.status_code == 404:
-            raise ValueError(f"Task gone")
-        response.raise_for_status()
-        result_json = response.json()
-        return [BeamResult.model_validate(result) for result in result_json]
+        return []
